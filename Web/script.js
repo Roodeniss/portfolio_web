05d7ee228011c7b6f869dc4c1b2c9463b3e3d466
Roodeniss <66141538+Roodeniss@users.noreply.github.com>
2023-01-04 09:48:06 -0300

    Cambios

--- Web/script.js
+++ Web/script.js
@@ -1,16 +1,16 @@
 
-const hamb = document.querySelector(".hamburger");
-const navMen = document.querySelector(".navBarMenu");
-const btn = document.querySelector(".navBtn");
+const hamb = document.querySelector('.hamburger');
+const navMen = document.querySelector('.navBarMenu');
+const btn = document.querySelector('.navBtn');
+
 
 btn.addEventListener("click", () => {
    hamb.classList.toggle("active");
    navMen.classList.toggle("active");
 })
 
-/*
+
 document.querySelectorAll(".linkMenu").forEach(n => n.addEventListener("click", () => {
    hamb.classList.remove("active");
    navMen.classList.remove("active");
 }))
-*/

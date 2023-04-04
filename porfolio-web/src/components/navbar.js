bb5279776a379191c333d2fd26f3608820d90ef7
Roodeniss <66141538+Roodeniss@users.noreply.github.com>
2023-04-04 00:55:21 -0300

    add: changes and create a navbar component with react boostrap

--- porfolio-web/src/components/navbar.js
+++ porfolio-web/src/components/navbar.js
@@ -11,8 +11,8 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">About me</Nav.Link>
+            <Nav.Link href="#link">Work</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
